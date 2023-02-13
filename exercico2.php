f93366e2b5822c3fd2f0f3139c962f468bf74dce
mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2023-02-13 16:20:45 -0300

    Add files via upload

---
+++ exercico2.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+     
+    <tittle> Exemplo 01 </title>
+
+    <body>
+         
+    <?php
+        echo "ola mundo que usa o php, esse é meu primeiro programa!";
+    ?>
+     
+</body>
+
+</html>
