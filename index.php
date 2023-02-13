51f404e0969a9084333586e4407efe70e7c86dde
mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2023-02-13 16:04:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+     
+    <title>PHP - Aula exemplo do Uso de variáveis </title>
+</head>
+<body>
+    <?php
+    //Declarando variavies
+    $var1 = 1; //inteiro
+    $varX = 9.50; //ponto flutuante
+    $varY =9.50; //ponto flutuante
+       $var2 = $varX + $varY; //recebendo resultado de uma expressão
+
+       $varSTR = "atribui string";  // variável texto – string     
+       $varletra = "a";  // variável caracter           
+       $varbool = true; // atribuição lógica
+      
+       echo "exemplo de uma aplicação em PHP";         
+       print "<P>";
+
+       echo "conteudo de var1:  ";            
+       echo $var1;           
+       print "<P>";    
+
+       echo "conteudo de varX:  ";          
+       echo $varX;      
+       print "<P>";
+
+       echo "conteudo de varY:  ";     
+       echo $varY;          
+       print "<P>";
+
+       echo "conteudo de var2:  ";       
+       echo $var2;         
+       print "<P>";
+        
+       echo "conteudo de varSTR:  ";            
+       echo $varSTR;          
+       print "<P>";
+
+       echo "conteudo de varletra:  ";
+
+       echo $varletra;
+       print "<P>";
+       echo "conteudo de varbool: ";
+       echo $varbool;
+       ?> 
+        </body>      
+             </html>       
+    
